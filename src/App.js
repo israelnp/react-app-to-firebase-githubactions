2e99259bf2fdc9730486a24eaccf46e90861d0fb
israelnp <israelpereira1984@gmail.com>
2023-08-15 13:20:36 -0300

    add msg elvenworks

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+         Olá Elvenworks, Segunda aplicação frontend no Firebase.
+        </p> 
         <a
           className="App-link"
           href="https://reactjs.org"
